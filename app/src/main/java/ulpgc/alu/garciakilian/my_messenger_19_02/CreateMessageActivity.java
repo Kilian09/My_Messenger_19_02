15b28e7b68d2a25811d9cfd8d9b0a9f3add993eb
KIlian <kilian.garcia106@alu.ulpgc.es>
2020-02-19 13:59:15 +0000

    T03: Hecho el messanger

--- app/src/main/java/ulpgc/alu/garciakilian/my_messenger_19_02/CreateMessageActivity.java
+++ app/src/main/java/ulpgc/alu/garciakilian/my_messenger_19_02/CreateMessageActivity.java
@@ -3,10 +3,13 @@ package ulpgc.alu.garciakilian.my_messenger_19_02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import static java.net.Proxy.Type.HTTP;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -23,21 +26,48 @@ public class CreateMessageActivity extends AppCompatActivity {
     }
 
 
-    private void initLayoutComponents(){
-        sendButton  = findViewById(R.id.sendButton);
+    private void initLayoutComponents() {
+        sendButton = findViewById(R.id.sendButton);
         mesageEditText = findViewById(R.id.mesageEditText);
     }
 
-    private void initLayoutContent(){
+    private void initLayoutContent() {
         sendButton.setText(R.string.send_button_text);
         mesageEditText.setHint(R.string.message_text);
     }
 
     public void onClickSendMessage(View view) {
-        //String messageText = mesageEditText.getText().toString();
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        //Intent intent = new Intent(this,ReceiveMessageActivity.class);
-        //intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE,messageText);
-        startActivity(intent);
+      sendMessage();
     }
+
+    public void goAnotherActivity() {
+        String messageText = mesageEditText.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+
+    }
+
+
+    public void goAnotherAPP() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+
+        // Always use string resources for UI text.
+        // This says something like "Share this photo with"
+        String title = getResources().getString(R.string.chooser_title);
+        // Create intent to show chooser
+        Intent chooser = Intent.createChooser(intent, title);
+
+            startActivity(chooser);
+
+    }
+
+
+    public void sendMessage() {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        startActivity(emailIntent);
+
+    }
+
 }
